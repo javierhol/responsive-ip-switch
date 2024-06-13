c8f7c0dfd2b16c90d7a8719aa4a5c51518539d5d
FractionOfCode <javierholguin596.com>
2024-06-13 09:12:44 -0500

    ADD: Chnages in new module for users

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,12 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
 import Calendar from './pages/Calendar';
-import Chart from './pages/Chart';
 import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
 //import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
-import Signin from './components/signin/Signin';
+import Signin from './components/Signin/Signin';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -34,15 +27,6 @@ function App() {
   ) : (
     <>
       <Routes>
-        {/* <Route
-          index
-          element={
-            <>
-              <PageTitle title="Responsive Ip Switch" />
-              <ECommerce />
-            </>
-          }
-        /> */}
         <Route
           index
           element={
@@ -65,7 +49,7 @@ function App() {
           path="/calendar"
           element={
             <>
-              <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <PageTitle title="Calendar " />
               <Calendar />
             </>
           }
@@ -79,29 +63,12 @@ function App() {
             </>
           }
         /> */}
-        <Route
-          path="/forms/form-elements"
-          element={
-            <>
-              <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <FormElements />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-layout"
-          element={
-            <>
-              <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <FormLayout />
-            </>
-          }
-        />
+
         <Route
           path="/tables"
           element={
             <>
-              <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <PageTitle title="Users" />
               <Tables />
             </>
           }
@@ -110,56 +77,12 @@ function App() {
           path="/settings"
           element={
             <>
-              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <PageTitle title="Settings" />
               <Settings />
             </>
           }
         />
-        <Route
-          path="/chart"
-          element={
-            <>
-              <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Chart />
-            </>
-          }
-        />
-        <Route
-          path="/ui/alerts"
-          element={
-            <>
-              <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Alerts />
-            </>
-          }
-        />
-        <Route
-          path="/ui/buttons"
-          element={
-            <>
-              <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Buttons />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signin"
-          element={
-            <>
-              <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <SignIn />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signup"
-          element={
-            <>
-              <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <SignUp />
-            </>
-          }
-        />
+
       </Routes>
     </>
   );
--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -1,17 +1,17 @@
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
-import TableOne from '../components/Tables/TableOne';
+//import TableOne from '../components/Tables/TableOne';
 import TableThree from '../components/Tables/TableThree';
-import TableTwo from '../components/Tables/TableTwo';
+//import TableTwo from '../components/Tables/TableTwo';
 import DefaultLayout from '../layout/DefaultLayout';
 
 const Tables = () => {
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Tables" />
+      <Breadcrumb pageName="Customers" />
 
       <div className="flex flex-col gap-10">
-        <TableOne />
-        <TableTwo />
+        {/* <TableOne />
+        <TableTwo /> */}
         <TableThree />
       </div>
     </DefaultLayout>
